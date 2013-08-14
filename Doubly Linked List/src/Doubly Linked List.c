08f333c817f3bb909e3d269aa288bc99267273aa
titu1994 <titu1994@gmail.com>
2013-08-14 22:43:43 +0530

    Finished Add, Display and Display in Reverse

--- Doubly Linked List/src/Doubly Linked List.c
+++ Doubly Linked List/src/Doubly Linked List.c
@@ -1,7 +1,7 @@
 /*
  ============================================================================
  Name        : Doubly.c
- Author      : 
+ Author      :
  Version     :
  Copyright   : Your copyright notice
  Description : Hello World in C, Ansi-style
@@ -11,6 +11,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct node{
+	int data;
+	struct node *next, *previous;
+}Node;
+
+
 int main(void) {
 
 
@@ -18,3 +24,57 @@ int main(void) {
 
 	return EXIT_SUCCESS;
 }
+
+
+void addElement(Node **p, int v){
+	Node *newNode = NULL;
+
+	newNode = (Node*) calloc(1, sizeof(Node));
+	newNode->data = v;
+	newNode->next = NULL;
+	newNode->previous = NULL;
+
+	if(*p == NULL){
+		*p = newNode;
+		(*p)->previous = NULL;
+	}
+	else{
+		Node *temp = *p;
+
+		while(temp->next != NULL){
+			temp = temp->next;
+		}
+
+		temp->next = newNode;
+		newNode->previous = temp;
+	}
+
+
+}
+
+void displayElements(Node *p){
+
+	while(p != NULL){
+		printf("%d\t",p->data);
+		p = p->next;
+	}
+	printf("\n");
+
+}
+
+void displayElementsInReverse(Node *p){
+
+	while(p->next != NULL){
+		p = p->next;
+	}
+
+	while(p != NULL){
+		printf("%d\t",p->data);
+		p = p->previous;
+	}
+
+	printf("\n");
+
+}
+
+
