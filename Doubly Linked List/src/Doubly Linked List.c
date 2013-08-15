fb4889fa3625d04934b30685e1d164c4b76720c5
titu1994 <titu1994@gmail.com>
2013-08-15 18:07:25 +0530

    Finished Delete Function of Doubly Linked List

--- Doubly Linked List/src/Doubly Linked List.c
+++ Doubly Linked List/src/Doubly Linked List.c
@@ -18,9 +18,71 @@ typedef struct node{
 
 
 int main(void) {
+	int choice = -1;
+	int element,pos, size;
+
+	Node *first = NULL;
+
+	void addElement(Node **p, int v);
+	void displayElements(Node *p);
+	void displayElementsInReverse(Node *p);
+	int deleteElement(Node **p, int v);
+
+	setbuf(stdout, NULL);
+	printf("Enter the choice : \n1 to add\n2 to print\n3 for display elements in reverse\n4 for delete an element\n");
+
+	do{
+		printf("Enter choice : ");
+		scanf("%d",&choice);
+
+		switch (choice) {
+			case 1:{
+				//Add an element
+
+				printf("Enter the element\n");
+				scanf("%d",&element);
+
+				addElement(&first, element);
+				break;
+			}
+			case 2:{
+				//Print the list
+
+				displayElements(first);
+				break;
+			}
+			case 3:{
+				//Print list in reverse
+
+				displayElementsInReverse(first);
+				break;
+			}
+			case 4:{
+				//Delete an element
+
+				printf("Enter the element\n");
+				scanf("%d",&element);
+
+				if(deleteElement(&first, element) == 1){
+					printf("Deleted\n");
+				}
+				else{
+					printf("Not deleted\n");
+				}
+
+				break;
+			}
+
+			default:{
+				choice = -1;
+			}
+		}
 
 
+	}while(choice != -1);
 
+
+	printf("End");
 
 	return EXIT_SUCCESS;
 }
@@ -77,4 +139,39 @@ void displayElementsInReverse(Node *p){
 
 }
 
+int deleteElement(Node **p, int v){
 
+	Node *temp,*previous;
+	temp = *p;
+
+	while(temp != NULL){
+
+		if(temp->data == v){
+			break;
+		}
+
+		previous = temp;
+		temp = temp->next;
+
+	}
+
+	if(temp  == NULL){
+		return 0;
+	}
+	else{
+
+		if(temp == *p){
+			*p = temp->next;
+			(*p)->previous = NULL;
+			free(temp);
+		}
+		else{
+			previous->next = temp->next;
+			temp->next->previous = temp->previous;
+			free(temp);
+		}
+
+		return 1;
+	}
+
+}
